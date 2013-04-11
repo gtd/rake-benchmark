7c6cb90109d1b01391e3a3bbd54822db814749b1
Eric Marden <eric@xentek.net>
2013-04-11 17:30:30 -0500

    alias Rake::Task.execute to wrap it w/ benchmarks
    - Outputs benchmark information for executed rake tasks
    - based on work by Gabe de Silveira, see:
      http://darwinweb.net/articles/benchmarking-rake-tasks-and-trivial-rails-testing-

--- lib/rake/benchmark.rb
+++ lib/rake/benchmark.rb
@@ -1,7 +1,14 @@
 require "rake/benchmark/version"
+require 'rake'
+require 'benchmark'
 
-module Rake
-  module Benchmark
-    # Your code goes here...
+class Rake::Task
+  def execute_with_benchmark(*args)
+    bench = Benchmark.measure do
+      execute_without_benchmark(*args)
+    end
+    puts "  #{name} --> #{bench}"
   end
+  alias_method :execute_without_benchmark, :execute
+  alias_method :execute, :execute_with_benchmark
 end
